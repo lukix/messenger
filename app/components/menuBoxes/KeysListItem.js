501475f7975647e627ddd88e543294cf69420ac4
lukix <lukix.95@gmail.com>
2018-03-30 22:06:44 +0200

    Add share button

--- app/components/menuBoxes/KeysListItem.js
+++ app/components/menuBoxes/KeysListItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import moment from 'moment'
+import SharedStyles from '../../others/SharedStyles'
 
 const style = {
 	publicKey: {
-		marginBottom: '6px',
+		marginBottom: '10px',
 	},
 	keyInputText: {
 		width: '100%',
@@ -12,12 +13,28 @@ const style = {
 		color: 'white',
 		margin: '1px 0',
 	},
+	shareButton: {
+		...SharedStyles.button,
+		background: 'white',
+		color: 'black',
+		height: '22px',
+		fontSize: '1rem',
+	},
+	topBar: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
 }
 const selectTarget = (event) => event.target.select()
 export default function KeysBox({ publicKey, createdDate }) {
 	const dateString = moment(createdDate).format('MMM Do, HH:mm:ss')
 	return <div style={style.publicKey}>
-		<div>{dateString}</div>
+		<div style={style.topBar}>
+			<span>{dateString}</span>
+			<button style={style.shareButton}><i className="fas fa-link"></i> Share URL</button>
+		</div>
 		<input
 			type="text"
 			style={style.keyInputText}

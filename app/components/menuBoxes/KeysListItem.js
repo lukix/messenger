cd49dd762c9f51cb22c7243d5ae7d553859fcd16
lukix <lukix.95@gmail.com>
2018-03-30 22:26:20 +0200

    Change styles to emotion

--- app/components/menuBoxes/KeysListItem.js
+++ app/components/menuBoxes/KeysListItem.js
@@ -1,43 +1,49 @@
 import React from 'react'
 import moment from 'moment'
-import SharedStyles from '../../others/SharedStyles'
+import { css } from 'react-emotion'
+import Colors from '../../others/Colors'
 
-const style = {
-	publicKey: {
-		marginBottom: '10px',
-	},
-	keyInputText: {
-		width: '100%',
-		background: 'transparent',
-		border: 'none',
-		color: 'white',
-		margin: '1px 0',
-	},
-	shareButton: {
-		...SharedStyles.button,
-		background: 'white',
-		color: 'black',
-		height: '22px',
-		fontSize: '1rem',
-	},
-	topBar: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
-}
+const mainClass = css`
+	margin-bottom: 10px
+	.keyInputText {
+		width: 100%
+		background: transparent
+		border: none
+		color: white
+		margin: 1px 0
+	}
+	.topBar {
+		display: flex
+		flex-direction: row
+		justify-content: space-between
+		align-items: center
+	}
+	.shareButton {
+		border: none
+		padding: 0 8px
+		cursor: pointer
+		border-radius: 4px
+		background: white
+		color: black
+		height: 22px
+		font-size: 0.9rem
+		&:hover {
+			background: ${Colors.mainLight}
+		}
+	}
+`
+
 const selectTarget = (event) => event.target.select()
 export default function KeysBox({ publicKey, createdDate }) {
 	const dateString = moment(createdDate).format('MMM Do, HH:mm:ss')
-	return <div style={style.publicKey}>
-		<div style={style.topBar}>
+	return <div className={mainClass}>
+		<div className="topBar">
 			<span>{dateString}</span>
-			<button style={style.shareButton}><i className="fas fa-link"></i> Share URL</button>
+			<button className="shareButton"><i className="fas fa-link"></i> Share URL</button>
 		</div>
 		<input
 			type="text"
-			style={style.keyInputText}
+			className="keyInputText"
 			value={publicKey}
 			onFocus={selectTarget}
 			readOnly
